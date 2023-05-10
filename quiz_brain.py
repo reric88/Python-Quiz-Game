a23bf7d6322723b86d5e82f3fa9c1adb015ec7cb
Eric Collard <ruffyreborn@gmail.com>
2023-05-10 18:37:21 -0500

    Update

--- quiz_brain.py
+++ quiz_brain.py
@@ -9,3 +9,11 @@ class Quiz_Brain:
         self.question_number = 0
         self.score = 0
         self.question_list = q_list
+
+    # 4c: Create a method that retrieves the question from the question list created in main.py
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        # 4d lists start at 0, so to display the correct question number for the next step, increase question number by 1
+        self.question_number += 1
+        # 4e Show the number_text and ask for the users answer
+        input(f'Q.{self.question_number}: {current_question.text}; True/False')
